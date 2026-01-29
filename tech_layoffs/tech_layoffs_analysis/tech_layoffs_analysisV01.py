7ce5e89d91c87ba0895d3a7d3881850db7509e13
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2026-01-29 20:52:49 +0100

    update v02

--- tech_layoffs/tech_layoffs_analysis/tech_layoffs_analysisV01.py
+++ tech_layoffs/tech_layoffs_analysis/tech_layoffs_analysisV01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('/tech_layoffs_csv/tech_layoffs_Q2_2024.csv')
+df = pd.read_csv('leaned_tech_layoffs.csv')
 
 # Display the first few rows of the DataFrame
 display(df.head())

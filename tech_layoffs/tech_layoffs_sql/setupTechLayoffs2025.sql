cbe4987b971a6bc0981d2e2dcdf2b04a4cf271e6
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2026-05-22 21:06:26 +0200

    update layoffs

--- tech_layoffs/tech_layoffs_sql/setupTechLayoffs2025.sql
+++ tech_layoffs/tech_layoffs_sql/setupTechLayoffs2025.sql
@@ -24,4 +24,18 @@ CREATE TABLE techLayoffs2025 (
 Drop table techLayoffs2025;
 
 COPY techLayoffs2025 (Nr, Company, Location_HQ, Region, USState, Country, Continent, Laid_Off, Date_layoffs, Percentage, Company_Size_before_Layoffs, Company_Size_after_Layoffs, Industry, Stage, Money_Raised_in_mil, Year, latitude, longitude)
-FROM '/DataScienceProjects/tech_layoffs/tech_layoffs_csv/tech_layoffs_til_2025.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/ulrike_imac_air/projects/DataScienceProjects/tech_layoffs/tech_layoffs_csv/tech_layoffs_til_2025.csv' DELIMITER ',' CSV HEADER;
+
+select * from techLayoffs2025;
+
+CREATE TABLE companies (
+	company Text primary key,
+    companycode Text,
+    Employees Integer,
+    Country Text
+);
+
+Drop table companies;
+
+COPY companies (company,companycode,Employees,Country)
+FROM '/Users/ulrike_imac_air/projects/DataScienceProjects/tech_layoffs/tech_layoffs_csv/companies2025.csv' DELIMITER ',' CSV HEADER;
